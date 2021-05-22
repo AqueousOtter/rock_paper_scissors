2ac2e606fc4f8f3d854efb0ec92364ec85f9ddf5
AqueousOtter <dustin. brown.k@gmail.com>
2021-05-22 11:02:39 -0400

    Setting up intial files for Odin Project

---
+++ README.md
@@ -0,0 +1,3 @@
+Odin project: Rock Paper Scissors
+
+Goal: Make a rock, paper, scissors game. Player vs computer. No UI (later)
---
+++ scripts.js
@@ -0,0 +1,13 @@
+/*  Rock, Paper, Scissors
+    Odin Project
+    Dustin Brown 05/22/2021
+*/
+
+//computer play function
+
+//play round function: two parameters case insensitive "playerSelection", "computerSelection"
+//return string winner
+
+// game function: loop using round function, 5 rounds, score, winner/loser at end
+//use prompt for input
+//console.log for results each round and end
